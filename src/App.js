22de8de49f4100f4ec4f9fd29a5274a6bb86e5d3
monstaro <codysmith2814@gmail.com>
2020-11-12 14:12:48 -0700

    Removes commented out code

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import Metronome from './components/Metronome/Metronome'
 import Header from './components/Header/Header';
 
 function App() {
+
   return (
     <div className="App">
       <Header />
-      {/* <img alt="fashionable gnome" src={METROGNOME} /> */}
-     <Metronome />
+      <Metronome />
     </div>
   );
 }
