473d5e85b10299504920ffb1a7e37b175d34a2c0
monstaro <codysmith2814@gmail.com>
2020-11-12 14:16:23 -0700

    Adds bootstrap styles

--- src/components/SavedTempos/SavedTempos.js
+++ src/components/SavedTempos/SavedTempos.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './SavedTempos.css';
+import ListGroup from 'react-bootstrap/ListGroup';
+
 const SavedTempos = (props) => {
     const { songName } = props;
     const  { tempo } = props;
     const { clickHandler } = props;
-    console.log(songName,  tempo)
+    const { currentTempo } = props;
+    console.log(currentTempo, parseInt(tempo))
+    const determineIfActive = () => {
+        if (currentTempo === parseInt(tempo)) {
+            return <ListGroup.Item as="li" className="song-name" active onClick={() => clickHandler('custom', tempo)} >
+            {songName} ({tempo} BPM)
+          </ListGroup.Item>
+        } else {
+            return <ListGroup.Item as="li" className="song-name" onClick={() => clickHandler('custom', tempo)} >
+            {songName} ({tempo} BPM)
+          </ListGroup.Item>
+        }
+    }
     return (
-        <div className="songs-container">
-<button className="song-btn" onClick={() => clickHandler('custom', tempo)}>{songName}</button>
-</div>
+    <>
+    <ListGroup as="ul" defaultActiveKey="#link1">
+    {determineIfActive()}
+    </ListGroup>
+</>
     )
 }
 
