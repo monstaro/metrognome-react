7f16fb3e5462bfe01072cdf0e56e5f17008a7cf5
monstaro <codysmith2814@gmail.com>
2020-11-12 09:51:43 -0700

    Removes old code

--- src/components/Metronome/Metronome.js
+++ src/components/Metronome/Metronome.js
@@ -36,7 +36,6 @@ class Metronome extends React.Component
           })
         }
     }
-
     scheduleNote = (beatNumber, time) =>
     {
         this.setState({
@@ -53,7 +52,6 @@ class Metronome extends React.Component
         osc.start(time);
         osc.stop(time + 0.03);
     }
-
     scheduler = () =>
     {
         while (this.state.nextNoteTime < this.state.audioContext.currentTime + this.state.scheduleAheadTime ) {
@@ -61,7 +59,6 @@ class Metronome extends React.Component
             this.nextNote();
         }
     }
-
     start = () => 
     {
         if (this.state.isRunning) return;
@@ -79,7 +76,6 @@ class Metronome extends React.Component
           intervalID: setInterval(() => this.scheduler(), this.state.lookahead)
         })
     }
-
     stop = () =>
     {
         this.setState({
@@ -87,7 +83,6 @@ class Metronome extends React.Component
         })
         clearInterval(this.state.intervalID);
     }
-
     startStop = () =>
     {
       console.log(this.state.tempo)
@@ -126,34 +121,6 @@ class Metronome extends React.Component
       }
       localStorage.setItem('songs', JSON.stringify(songs));
     }
-    checkTempo = () => {
-      console.log(JSON.parse(localStorage.getItem('songs')))
-      if (this.state.tempo < 120) {
-        this.setState({
-          message: 'This be some love making music'
-        })
-      }
-      if (this.state.tempo >= 120 && this.state.tempo < 139) {
-          this.setState({
-            message: 'DIS BOPS DOE'
-          })
-      }
-      if (this.state.tempo >= 139 && this.state.tempo < 159) {
-        this.setState({
-          message: 'We rarely play in this tempo range. Challenge accepted?'
-        })
-      }
-      if (this.state.tempo >= 159 && this.state.tempo < 180) {
-        this.setState({
-          message: 'Get in the zone, your comfort zone'
-        })
-      }
-      if (this.state.tempo >= 181) {
-        this.setState({
-          message: 'You drank a lot of coffee...'
-        })
-      }
-    }
 render() {
   return (
           <div className="metronome">
@@ -170,7 +137,7 @@ render() {
               value={this.state.tempo}
               onChange={(e) => this.setState({
                 tempo: e.target.value
-              }, this.checkTempo())}
+              })}
             />
             <img className="tempo-change" src={plus} alt="increase BPM by 1" onClick={() => {
               this.tweakTempo('add')
@@ -196,108 +163,6 @@ render() {
             </div>
           </div>
         )
+  }
 }
-}
-
 export default Metronome;
-
-// const Metronome = () => {
-//   const [bpm, setBpm] = useState(120);
-//   const [playing, setPlaying] = useState(false);
-//   const [count, setCount] = useState(0);
-//   const [beatsInQueue, setBeats] = useState([]);
-//   const [nextNoteTime, setNextTime] = useState(0.0);
-//   const [audioContext, setAudioContext] = useState(null);
-//   const [intervalID, setIntervalID] = useState(null);
-  
-//   const nextNote = () => {
-//     let secondsPerBeat = 60 / bpm;
-//     setNextTime(secondsPerBeat);
-//     setCount(count+1);
-//     count === 4 && setCount(0);
-//   }
-
-//   const scheduleNote = (beatNumber, time) => {
-//     setBeats([...beatsInQueue, {note: beatNumber, time: time}])
-
-//     const osc = audioContext.createOscillator();
-//     const envelope = audioContext.createGain();
-        
-//     osc.frequency.value = (beatNumber % 4 === 0) ? 1000 : 800;
-//     envelope.gain.value = 1;
-//     envelope.gain.exponentialRampToValueAtTime(1, time + 0.001);
-//     envelope.gain.exponentialRampToValueAtTime(0.001, time + 0.02);
-
-//     osc.connect(envelope);
-//     envelope.connect(audioContext.destination);
-    
-//     osc.start(time);
-//     osc.stop(time + 0.03);
-// }
-
-// const scheduler = () => {
-//     // while there are notes that will need to play before the next interval, schedule them and advance the pointer.
-//     while (nextNoteTime < AudioContext.currentTime + 0.1 ) {
-//         scheduleNote(count, nextNoteTime);
-
-//         // Advance current note and time by a quarter note (crotchet if you're posh)
-//         setNextTime(60.0 / bpm); // Add beat length to last beat time
-//         setCount(count+1);    // Advance the beat number, wrap to zero
-//         if (count === 4) {
-//             setCount(0);
-//             nextNote();
-//         }
-//     }
-// }
-
-// const start = () =>
-// {
-//     if (playing) return;
-
-//     if (audioContext === null)
-//     {
-//       console.log('k')
-//         setAudioContext(new(window.AudioContext || window.webkitAudioContext)());
-//         console.log(audioContext, AudioContext)
-//     }
-
-//     setPlaying(true);
-//     setBeats(0);
-//     setNextTime(AudioContext.currentTime + 0.05);
-//     setIntervalID(setInterval(() => scheduler(), 25))
-// }
-
-// const stop = () =>
-// {
-//     setPlaying(false)
-//     setInterval(intervalID);
-// }
-
-// const startStop = () => {
-//     if (playing) {
-//         stop();
-//     }
-//     else {
-//         start();
-//     }
-// }
-//     return (
-//       <div className="metronome">
-//         {bpm}
-//         <input
-//           type="range"
-//           min="60"
-//           max="240"
-//           value={bpm}
-//           onChange={(e) => setBpm(e.target.value)}
-//         />
-//         <button onClick={() => startStop()}>
-//           {playing ? 'Stop' : 'Start'}
-//         </button>
-//         Metronome Component
-//       </div>
-//     )
-// }
-
-
-// export default Metronome;
